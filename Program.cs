660e1d5fdb4f4653dad838cadb2ea5442831883d
Luka <luka07765@gmail.com>
2025-03-13 21:10:36 +0100

    cross origin

--- Program.cs
+++ Program.cs
@@ -152,7 +152,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy("AllowAll", policy =>
     {
         policy
-            .WithOrigins("https://localhost:3000") // Must match the frontend URL with https
+            .WithOrigins("*") // Must match the frontend URL with https
             .AllowAnyHeader()
             .AllowAnyMethod()
             .AllowCredentials();
