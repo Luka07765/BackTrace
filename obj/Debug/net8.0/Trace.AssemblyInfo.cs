8c4fba7f5c569cf559413298ae5e1a5eb063ff03
unknown <luka07765@gmail.com>
2025-10-27 22:53:44 +0100

    easdf

--- obj/Debug/net8.0/Trace.AssemblyInfo.cs
+++ obj/Debug/net8.0/Trace.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Trace")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e4d6c6721844a8d5028b2def7956f745aba05a37")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b5e2e74027be7e4ddbdd55ddceb9be183fa6ece")]
 [assembly: System.Reflection.AssemblyProductAttribute("Trace")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Trace")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
