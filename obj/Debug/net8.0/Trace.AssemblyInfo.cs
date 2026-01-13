27df2742dbf1deabf7bdca6041772582671a199e
unknown <luka07765@gmail.com>
2026-01-13 22:41:16 +0100

    org

--- obj/Debug/net8.0/Trace.AssemblyInfo.cs
+++ obj/Debug/net8.0/Trace.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Trace")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a20db4aea5e3bca4ad7a087e0644b36a9e15f04")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a57b703c74b5fbd44ec9983ba6a081175c527ff1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Trace")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Trace")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
